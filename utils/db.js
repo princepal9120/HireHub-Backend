5e408e3c94e17c4ddea21ccc946bbc22a456c419
Prince Pal <princepal9120@gmail.com>
2024-08-26 23:34:00 +0530

    job website backen

---
+++ utils/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('mongodb connected successfully');
+    } catch (error) {
+        console.log("Mongodb not connected successfully",error);
+    }
+}
+export default connectDB;
